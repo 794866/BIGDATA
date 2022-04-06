fc624785afcd9d1b5b97dd2a721bf58a17bb2f3d
uri <794866@unizar.es>
2022-04-06 19:26:27 +0200

    generado insert de actores (excluido errores)

--- src/main/java/DataProcessing/EquipoProduccion/ProcesandoActores.java
+++ src/main/java/DataProcessing/EquipoProduccion/ProcesandoActores.java
@@ -4,6 +4,7 @@ package DataProcessing.EquipoProduccion;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
@@ -16,7 +17,6 @@ public class ProcesandoActores {
 
     static final String inputFile="procesandoActores.csv";
     static int count = 1;
-    static String movieID;
     static List<String> split;
     static ArrayList<String> rows = new ArrayList();
 
@@ -36,38 +36,44 @@ public class ProcesandoActores {
                             System.out.println(count);
                             count++;
                             String newLine = line.replace("\"[{'cast_id': ","")
-                                    .replace("{'cast_id': ","")
-                                    .replace("'character': '","")
-                                    .replace("'credit_id': '","")
-                                    .replace("'gender': ","")
-                                    .replace("'name': '","")
-                                    .replace("'id': ","")
+                                    .replace("{'cast_id'","")
+                                    .replace("'cast_id'","")
+                                    .replace("'character'","")
+                                    .replace("'credit_id'","")
+                                    .replace("'gender'","")
+                                    .replace("'name'","")
+                                    .replace("'id'","")
+                                    .replace("'order'","")
+                                    .replace("'profile_path'","")
                                     .replaceAll("\"\"", "")
-                                    .replaceAll("\"", "")
-                                    .replaceAll("\'", "");
+                                    .replaceAll("\"", "");
 
                             split = List.of(newLine.split("},"));
                             for(int i=0;i<split.size();i++) {
                                 String[] campos = split.get(i).split(",");
 
-
+                                if(campos[2].replace(",","").replace("'","").replace(":","").trim().length() != 24){
+                                    throw new RuntimeException();
+                                }
                                 if(!rows.contains(campos[2])){
                                     rows.add(campos[2]);
                                     String newInsert = "INSERT INTO Actor("+
                                             "ID,papel,nombre,genero,pelicula)"+
                                             "VALUES('"
-                                            + campos[2].trim() +
-                                            "','" + campos[1].trim() +
-                                            "','" + campos[5].trim() +
-                                            "'," + campos[3].trim() +
+                                            + campos[2].replace(",","").replace("'","").replace(":","").trim() +
+                                            "','" + campos[1].replace(",","").replace("'","").replace(":","").trim() +
+                                            "','" + campos[5].replace(",","").replace("'","").replace(":","").trim() +
+                                            "'," + campos[3].replace(",","").replace("'","").replace(":","").trim() +
                                             "," + movieID.trim() + ");";
                                     fileWritter(newInsert, "inserts_actores.sql");
                                 }
                             }
                         }
+                    } catch (RuntimeException e){
+                        fileWritter(String.valueOf(count), "allErrors.txt");
                     } catch (Exception exception) {
-                        fileWritter(String.valueOf(count), "errors.txt");
-                        line = br.readLine();
+                        fileWritter(String.valueOf(count), "allErrors.txt");
+                        //line = br.readLine();
                     }
                 }
                 line = br.readLine();
@@ -78,8 +84,5 @@ public class ProcesandoActores {
         }
     }
 
-    public static void ordenarCampos(String[] campos){
-
-    }
 }
 
